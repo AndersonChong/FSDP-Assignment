75fd032b59f2f207e1743e43b75103b6e8384be6
YEYINTAUNG-21 <S10268975@connect.np.edu.sg>
2025-11-11 23:11:06 +0800

    finish creat agent pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import CreateAgentPage from "./pages/CreateAgent";
 import UserForm from "./components/UserForm";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/create-agent" element={<CreateAgentPage />} />
         <Route path="/test" element={<UserForm />} />
       </Routes>
     </Router>
