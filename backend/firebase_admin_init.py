ae7b7de54c2355bab0cfb8e7f7b034bdc641d5b5
YEYINTAUNG-21 <S10268975@connect.np.edu.sg>
2026-01-09 16:19:37 +0800

    Fix Frontend, add media queries for for different devices

--- backend/firebase_admin_init.py
+++ backend/firebase_admin_init.py
@@ -1,17 +1,13 @@
 import os
 import firebase_admin
 from firebase_admin import credentials, firestore
-from dotenv import load_dotenv
 
-load_dotenv()  # load .env
-
-cred_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
-if not cred_path or not os.path.exists(cred_path):
-    raise FileNotFoundError(f"Firebase credentials not found at {cred_path}")
-
-cred = credentials.Certificate(cred_path)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+cred_path = os.path.join(BASE_DIR, "firebase", "serviceaccount.json")
 
 if not firebase_admin._apps:
+    cred = credentials.Certificate(cred_path)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
